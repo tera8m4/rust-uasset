621b17c18c0d99095353c2ba0e43245bc75b0fc6
tera8m4 <82479019+tera8m4@users.noreply.github.com>
2025-06-17 16:01:55 +0300

    add basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, fs::File, io::Seek, process};
+
+use rust_uasset::{data::data_table::DataTable, uasset_parser::UassetParser};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <filename>", args[0]);
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let file = File::open(&file_path).unwrap();
+    let mut parser = UassetParser::new(file);
+    parser.parse_asset().expect("managed to parse the asset");
+
+    parser
+        .reader
+        .seek(std::io::SeekFrom::Start(
+            parser.entries[1].serial_offset as u64,
+        ))
+        .expect("failed to seek");
+
+    let data_table: DataTable = parser.read().expect("Failed to parse data table");
+
+    println!("{:#?}", data_table.rows);
 }
